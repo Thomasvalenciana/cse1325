99de5147c8ff1ba95786e24aa18c50de447e37aa
Thomas Valenciana <thomas.valenciana@mavs.uta.edu>
2023-01-18 22:16:23 -0600

    P01 bonus

---
+++ P01/bonus/Hello.java
@@ -0,0 +1,20 @@
+/*
+ * Thomas Valenciana 1001795919
+ */
+
+import java.util.Scanner;
+
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        Scanner in =new Scanner(System.in);
+        System.out.println("Hello,");
+        String name = in.nextLine();
+        System.out.print("Hello, ");
+        System.out.print(name);
+             
+	}
+	
+}
+	
