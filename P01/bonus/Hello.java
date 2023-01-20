aa481cbdeb96249c411bf5c50d00ce5c38565dc8
Thomas Valenciana <thomas.valenciana@mavs.uta.edu>
2023-01-20 01:44:55 -0600

    P01 Bonus

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -9,10 +9,11 @@ public class Hello
     public static void main(String[] args)
     {
         Scanner in =new Scanner(System.in);
-        System.out.println("Hello,");
+        System.out.print("What is your name? ");
         String name = in.nextLine();
         System.out.print("Hello, ");
         System.out.print(name);
+        System.out.print("!");
         System.out.print("\n");
              
 	}
