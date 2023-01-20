600fccc4a69a75d04bcb19c1e632e3ee679016d4
Thomas Valenciana <thomas.valenciana@mavs.uta.edu>
2023-01-20 01:13:06 -0600

    P01 Full Credit

---
+++ P01/full_credit/Hello.java
@@ -0,0 +1,14 @@
+ /*
+  * Thomas Valenciana 1001795919
+  */
+  
+  public class Hello
+ {
+	  public static void main(String[] args)
+	  {
+		  
+		  System.out.println("Hello, Thomas Valenciana!");
+		  System.out.printf("\n");
+	  }
+ }
+   
