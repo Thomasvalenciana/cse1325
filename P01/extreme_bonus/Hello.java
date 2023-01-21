a5b99b06643c5f503601dd8f799314e1b0422923
Thomas Valenciana <thomas.valenciana@mavs.uta.edu>
2023-01-21 02:08:41 -0600

    P01 Extreme Bonus

---
+++ P01/extreme_bonus/Hello.java
@@ -0,0 +1,12 @@
+/*
+  * Thomas Valenciana 1001795919
+  */
+  
+  public class Hello
+ {
+	  public static void main(String[] args)
+	  {
+		 String name = System.getProperty("user.name");
+	     System.out.println("Hello, " + name + "!");
+	  }
+ }
