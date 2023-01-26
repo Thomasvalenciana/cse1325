f71eb1968ca76a991557417673108a893bc2aac9
Thomas Valenciana <thomas.valenciana@mavs.uta.edu>
2023-01-26 13:01:00 -0600

    P02 Full Credit

---
+++ P02/full_credit/Pet.java
@@ -0,0 +1,41 @@
+
+ 
+public class Pet {
+
+private  String name ;
+private  double age;
+private Type type;
+
+
+////String[] name ;
+//name = new String []= {"Pony Boy",  "Sonic ","Orochimaru","Benito"};
+
+//double []age = new double {8,1, 9, 8} ;
+
+
+
+public class Pet(String name, double age, Type type)
+ {
+  this.name= name;
+  this.age= age;
+  this.type= type;
+ }
+ 
+ 
+ @Override 
+ public String toString()
+ {
+   
+  return name + "   is a  "+ type + " they are" age + "old" ;
+ }
+ 
+ 
+ }
+
+ //HORSE= "PonyBoy",  HEDGEHOG= " Sonic ", SNAKE= "Orochimaru" , //
+//final double
+//horse
+//hedgehog
+//snake
+//bunny
+//age double
---
+++ P02/full_credit/Type.java
@@ -0,0 +1,7 @@
+public enum Type
+{
+  HORSE,
+   HEDGEHOG,
+   SNAKE,
+   BUNNY
+ }
